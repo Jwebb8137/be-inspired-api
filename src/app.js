a6541d73d2dcf2537c1b2bbc1de47144c28ac985
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-11-23 17:27:10 -0500

    initial

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 const express = require('express')
+const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-
-const app = express()
 
 const morganOption = (process.env.NODE_ENV === 'production')
   ? 'tiny'
