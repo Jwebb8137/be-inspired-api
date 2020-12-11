119b6b42bb2e1ab5c5d7246f262e6457ede67a5b
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-12-10 18:27:08 -0800

    user login

--- src/app.js
+++ src/app.js
@@ -52,14 +52,14 @@ app.post('/api/media', async (req, res) => {
 
 //JWT VERIFICATION
 
-app.get("/api/is-verified", authorization, async (req, res) => {
-  try {
-    res.json(true); 
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error")
-  }
-})
+// app.get("/api/is-verified", authorization, async (req, res) => {
+//   try {
+//     res.json(true); 
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("Server Error")
+//   }
+// })
 
 // knexInstance('users').select('*')
 //     .then(result => {
