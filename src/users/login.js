7778f35194b0d17053cdb7f11c4625e0d0a9f6c9
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-12-11 20:10:42 -0800

    login

--- src/users/login.js
+++ src/users/login.js
@@ -8,17 +8,17 @@ router.post("/", async (req, res) => {
     //1. destructure the req.body
     const {username, passwordInput} = req.body;
     //2. check if user doesn't exit (if not throw error)
-    const user = await pool.query("SELECT * FROM profiles WHERE username = $1", [username])
+    const user = await pool.query("SELECT * FROM users WHERE username = $1", [username])
     if (user.rows.length === 0) {
       return res.status(401).send("Password or username does is incorrect")
     }
     //3. check if password is correct
-    const validPassword = await bcrypt.compare(passwordInput, user.rows[0].password);
+    const validPassword = await bcrypt.compare(passwordInput, user.rows[0].user_password);
     if(!validPassword) {
       return res.status(401).send("Password or Email is incorrect")
     };
     //4. issue jwt token
-    const token = jwtGenerator(user.rows[0].user_id);
+    const token = jwtGenerator(user.rows[0].id);
     res.json({ token })
   } catch (err) {
     console.error(err.message)
