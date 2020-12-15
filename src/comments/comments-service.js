3d3340af8565b198a3f964a153a2998337b31642
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-12-14 20:58:20 -0800

    join

--- src/comments/comments-service.js
+++ src/comments/comments-service.js
@@ -4,7 +4,7 @@ const CommentsService = {
       .select('*')
       .from('comments')
       .where('post_id', id)
-      .join('users', id, 'users.id')
+      .join('users', 'comments.id', 'users.id')
   },
 
   insertComment(knex, newComment) {
